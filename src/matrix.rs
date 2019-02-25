49e865d1eaf8d8ba5697a3d91f67dfbdcf4e1871
faudil.puttilli <faudil.puttilli@epitech.eu>
2019-02-25 17:33:05 +0100

    Impl fmt::debug for Matrix and scalar multiplication added

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,18 @@
+use std::fmt;
 use std::ops::{Index, Mul};
-use std::cmp::{Eq, PartialEq};
+use std::cmp::{PartialEq};
 
 pub struct Matrix {
     _height: usize,
     _width: usize,
     _value: Vec<f64>
+}
+
+impl fmt::Debug for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let str_list = self._value.iter().map(|&x| x.to_string()).collect::<Vec<String>>();
+        write!(f, "Matrix :: height = {} width = {}\n value = [{}]", self._height, self._width, str_list.join(", "))
+    }
 }
 
 impl Index<usize> for Matrix {
@@ -14,31 +22,45 @@ impl Index<usize> for Matrix {
     }
 }
 
+
 impl PartialEq for Matrix {
     fn eq(&self, other: &Matrix) -> bool {
         if self._height * self._width != other._height * other._width {
             return false;
         }
         for i in 0..self._width * self._height {
-            if self._value[i] - other._value[i] > 0.0001 {
+            if (self._value[i] - other._value[i]).abs() > 0.0001 {
                 return false;
             }
         }
-        true
+        return true
     }
 }
 
-impl Eq for Matrix {
+impl Mul<f64> for Matrix {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        let mut offset;
+        let mut res = self.clone();
+        for i in 0..self._height {
+            offset = i * res._width;
+            for j in 0..self._width {
+                res._value[offset + j] *= other;
+            }
+        }
+        res
+    }
 }
 
-impl Mul for Matrix {
+impl Mul<Matrix> for Matrix {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self {
         let mut offset;
         let mut res = Matrix::new(self._height, other._width);
         for i in 0..self._height {
-            offset = i * res._height;
+            offset = i * res._width;
             for j in 0..self._width {
                 res._value[offset + j] = self.mul_line(&other, i, j);
             }
@@ -46,6 +68,7 @@ impl Mul for Matrix {
         res
     }
 }
+
 
 impl Matrix {
     pub fn new(height: usize, width: usize) -> Matrix {
@@ -62,35 +85,34 @@ impl Matrix {
 
     fn mul_line(&self, other: &Matrix, i: usize, j: usize) -> f64{
         let mut res = 0.0;
-        for y in i..self._height - 1 {
-            for x in j..other._width - 1 {
-                res += self._value[y * self._height + x] * other._value[x * self._height + y]
+            for k in 0..other._width {
+                res += self._value[i * self._width + k] * other._value[k * self._width + j]
             }
-        }
         res
     }
 
     pub fn add(&mut self, other: &Matrix) {
-        let mut offset;
-        let mut index;
-        for y in 0..self._height {
-            offset = y * self._height;
-            for x in 0..self._width {
-                index = offset + x;
-                self._value[index] += other[index];
-            }
+        let bound = self._height * self._width;
+        for i in 0..bound {
+            self._value[i] += other._value[i];
         }
     }
 
     pub fn sub(&mut self, other: &Matrix) {
-        let mut offset;
-        let mut index;
-        for y in 0..self._height {
-            offset = y * self._height;
-            for x in 0..self._width {
-                index = offset + x;
-                self._value[index] -= other[index];
-            }
+        let bound = self._height * self._width;
+        for i in 0..bound {
+            self._value[i] -= other._value[i];
         }
+
+    }
+
+    pub fn to_string(&self) -> String {
+        let str_list = self._value.iter().map(|&x| x.to_string()).collect::<Vec<String>>();
+        format!("h = {} w = {} value = [{}]", self._height, self._width, str_list.join(", "))
+    }
+
+    pub fn clone(&self) -> Matrix {
+        let a = self._value.clone();
+        Matrix::new_init(self._height, self._width, a)
     }
 }
--- tests/matrix_tests.rs
+++ tests/matrix_tests.rs
@@ -13,7 +13,7 @@ mod matrix_tests {
         let n = Matrix::new_init(2, 2, vec![1.0, 1.0, 2.0, 2.0]);
         let r = Matrix::new_init(2, 2, vec![2.0, 2.0, 4.0, 4.0]);
         m.add(&n);
-        assert_eq!(m.eq(&r), true);
+        assert_eq!(m, r);
     }
 
     #[test]
@@ -22,15 +22,24 @@ mod matrix_tests {
         let n = Matrix::new_init(2, 2, vec![1.0, 1.0, 2.0, 2.0]);
         let r = Matrix::new_init(2, 2, vec![0.0, 0.0, 0.0, 0.0]);
         m.sub(&n);
-        assert_eq!(m.eq(&r), true);
+        assert_eq!(m, r);
     }
 
     #[test]
     fn simple_multiplication() {
-        let m = Matrix::new_init(2, 2, vec![1.0, 1.0, 0.0, 1.0]);
-        let n = Matrix::new_init(2, 2, vec![2.0, 1.0, 1.0, 1.0]);
-        let r = Matrix::new_init(2, 2, vec![3.0, 2.0, 1.0, 1.0]);
+        let m = Matrix::new_init(2, 2, vec![1.0, 1.0,
+                                                                      0.0, 1.0]);
+        let n = Matrix::new_init(2, 2, vec![2.0, 1.0,
+                                                                      1.0, 1.0]);
+        let r = Matrix::new_init(2, 2, vec![3.0, 2.0,
+                                                                      1.0, 1.0]);
         let t = m * n;
-        assert_eq!(t.eq(&r), true);
+        assert_eq!(t, r);
+    }
+
+    #[test]
+    fn simple_clone() {
+        let a = Matrix::new_init(2, 2, vec![2.0, 3.0, 5.0, 7.25]);
+        assert_eq!(a, a.clone());
     }
 }
