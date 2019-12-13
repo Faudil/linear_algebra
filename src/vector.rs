a244c01f70c48d3e5ee3802613557855fd026980
faudil.puttilli <faudil.puttilli@epitech.eu>
2019-12-13 12:21:10 +0100

    compilation error fixed in vector.rs vector.rs has regressed, will be implemented later

--- src/vector.rs
+++ src/vector.rs
@@ -1,12 +1,11 @@
-use std::ops::{Index, Mul};
 
-impl Index<f64> for Vector {
+/*impl Index<f64> for Vector {
     type Output = ();
 
-    fn index(&self, index: usize) -> &f64 {
+    fn index(&self, index: f64) -> &f64 {
         &self._value[index]
     }
-}
+}*/
 
 pub struct Vector {
     _size: usize,
@@ -16,6 +15,6 @@ pub struct Vector {
 
 impl Vector {
     pub fn new(size: usize) -> Vector {
-        Vector {_size: size, _value:ù** vec![0.0; size]}
+        Vector {_size: size, _value: vec![0.0; size]}
     }
 }
